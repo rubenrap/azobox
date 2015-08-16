cb9e30168600596c953fae2da1486894915e916e
rubenrap <rubenrap@Apollo.lan>
2015-08-16 19:31:08 +0000

    Xcode and Github - test 2.

---
+++ Test-XcodeGithub/Test-XcodeGithub/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test-XcodeGithub
+//
+//  Created by rubenrap on 8/16/15.
+//  Copyright (c) 2015 rubenrap. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Testing Xcode and Github as a Xcode project\n");
+    return 0;
+}
