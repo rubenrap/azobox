fc5c9394496ca17ec40981bd1360dff20da1813b
rubenrap <rubenrap@Apollo.lan>
2015-08-16 18:36:51 +0000

    Commit test for Xcode and Git!

---
+++ HelloXcode/HelloXcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloXcode
+//
+//  Created by rubenrap on 8/16/15.
+//  Copyright (c) 2015 rubenrap. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Xcode!\n");
+    return 0;
+}
