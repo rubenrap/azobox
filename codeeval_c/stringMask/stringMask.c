644157a4fbdbf0609acff9d7bea12cb844c9ee3c
rubenrap <rubenrap@Apollo.lan>
2015-09-15 23:00:31 +0000

    Change in the repository's organization. Each solution corresponds to a self-describing file.

---
+++ codeeval_c/stringMask/stringMask.c
@@ -0,0 +1,14 @@
+//
+//  stringMask.c
+//
+//  Created by rubenrap on 8/30/15.
+//  Copyright (c) 2015 rubenrap. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("String Mask!\n");
+    return 0;
+}
